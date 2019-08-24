8716ced4e710cce95c53a1dbfd044c9518c9803e
hiroshi.matsui <hiroshi.matsui@STTA18006-Hiroshi-Matsui.local>
2019-08-24 22:27:02 +0900

    Rename: slack_report -> slack_reporter

--- smtools/extensions/__init__.py
+++ smtools/extensions/__init__.py
@@ -1,4 +1,2 @@
-from smtools.extensions.slack_reporter import slack_report as slack_report
-from smtools.extensions.snapshot_transfer import (
-    snapshot_transfer as snapshot_transfer,
-)
+from smtools.extensions.slack_reporter import slack_reporter
+from smtools.extensions.snapshot_transfer import snapshot_transfer
