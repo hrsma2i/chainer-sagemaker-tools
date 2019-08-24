9554b3f5632c48fae2d03a0c4d919e1e5e609c66
hiroshi.matsui <hiroshi.matsui@STTA18006-Hiroshi-Matsui.local>
2019-08-25 00:52:07 +0900

    Make types.py

---
+++ smtools/types.py
@@ -0,0 +1,19 @@
+import json
+
+
+def chainer_trigger(s):
+    n, unit = s[:-1], s[-1]
+    unit_name = {"e": "epoch", "i": "iteration"}
+    assert (
+        unit in unit_name
+    ), "The last character of a trigger must be `e` or `i`"
+    return (int(n), unit_name[unit])
+
+
+def list_from_str(s):
+    if type(s) == list:
+        return s
+    elif type(s) == str:
+        return json.loads(s.replace("'", '"'))
+    else:
+        raise ValueError
