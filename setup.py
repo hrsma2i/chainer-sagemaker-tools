085066fb02cbca6033c7c274e5144839cf7ec78b
hrsma2i <hrs.ma2i@gmail.com>
2020-01-11 16:52:44 +0900

    Generate setup.py from poetry build.

--- setup.py
+++ setup.py
@@ -1,24 +1,47 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sage_extensions', 'sagemaker_tools', 'smtools', 'smtools.extensions']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Pillow>=7.0.0,<8.0.0',
+ 'boto3>=1.11.0,<2.0.0',
+ 'chainer==6.0.0',
+ 'joblib>=0.14.1,<0.15.0',
+ 'matplotlib>=3.1.2,<4.0.0',
+ 'numpy>=1.18.1,<2.0.0',
+ 'pandas>=0.25.3,<0.26.0',
+ 'pyyaml>=5.3,<6.0',
+ 'sagemaker>=1.50.1,<2.0.0',
+ 'slackweb>=1.0.5,<2.0.0',
+ 'tqdm>=4.41.1,<5.0.0']
+
+entry_points = \
+{'console_scripts': ['mgconf = smtools.merge_configs:main',
+                     'smbatch = sagemaker_tools.batch_inference:main',
+                     'smdeploy = sagemaker_tools.deploy_endpoint:main',
+                     'smtrain = sagemaker_tools.exec_train:main']}
+
+setup_kwargs = {
+    'name': 'smtools',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'hrsma2i',
+    'author_email': 'hrs.ma2i@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
 
 
-with open("requirements.txt") as requirements_file:
-    install_requires = requirements_file.read().splitlines()
-
-setup(
-    author="tn1031",
-    author_email="ttt.nakamura1031@gmail.com",
-    name="smtools",
-    description="Some extensions and tools to run Chainer jobs on Amazon SageMaker",
-    version="0.1.8.4",
-    packages=find_packages(),
-    install_requires=install_requires,
-    entry_points={
-        "console_scripts": [
-            "smtrain=sagemaker_tools.exec_train:main",
-            "smdeploy=sagemaker_tools.deploy_endpoint:main",
-            "smbatch=sagemaker_tools.batch_inference:main",
-            "mgconf=smtools.merge_configs:main",
-        ]
-    },
-    license="MIT license",
-)
+setup(**setup_kwargs)
