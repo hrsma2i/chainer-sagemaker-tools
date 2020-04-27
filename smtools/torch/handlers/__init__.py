3d3c361b2ed1b485ab5620dadff0aa08643075e5
hrsma2i <hrs.ma2i@gmail.com>
2020-04-27 18:58:13 +0900

    .

--- smtools/torch/handlers/__init__.py
+++ smtools/torch/handlers/__init__.py
@@ -2,3 +2,4 @@
 # is that we want to separetly specify each key_prefix on S3.
 from smtools.torch.handlers.archive import archive
 from smtools.torch.handlers.s3_copy import s3_copy
+from smtools.torch.handlers.s3_copy import remove
